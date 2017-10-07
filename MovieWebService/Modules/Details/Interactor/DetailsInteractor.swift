dff3ae1faf57f12c909045df5835119aec21fd81
zuchie <zuchie@gmail.com>
2017-10-07 04:15:40 -0700

    Refactor code

--- MovieWebService/Modules/Details/Interactor/DetailsInteractor.swift
+++ MovieWebService/Modules/Details/Interactor/DetailsInteractor.swift
@@ -11,14 +11,16 @@ class DetailsInteractor: NSObject, DetailsInteractorInput, TappableLabelDelegate
     weak var output: DetailsInteractorOutput!
     var view: UIView!
     
+    typealias LabelParams = (UILabel, CGRect, String, CGFloat)
+    
     enum Label {
-        case directorName(UILabel, CGRect, String, CGFloat)
-        case directorNameValue(UILabel, CGRect, String, CGFloat)
-        case tapToShowMore(TappableLabel, CGRect, String, CGFloat)
-        case actorName(UILabel, CGRect, String, CGFloat)
-        case actorNameValue(UILabel, CGRect, String, CGFloat)
-        case actorScreenName(UILabel, CGRect, String, CGFloat)
-        case actorScreenNameValue(UILabel, CGRect, String, CGFloat)
+        case directorName(LabelParams)
+        case directorNameValue(LabelParams)
+        case tapToShowMore(LabelParams)
+        case actorName(LabelParams)
+        case actorNameValue(LabelParams)
+        case actorScreenName(LabelParams)
+        case actorScreenNameValue(LabelParams)
     }
     
     var labels: [UILabel]!
@@ -42,34 +44,34 @@ class DetailsInteractor: NSObject, DetailsInteractorInput, TappableLabelDelegate
         let actor = director.film.cast[0]
         
         labels = [
-            Label.directorName(UILabel(),
+            Label.directorName((UILabel(),
                                labelFrame,
                                "Director Name",
-                               labelTextSize),
-            Label.directorNameValue(UILabel(),
+                               labelTextSize)),
+            Label.directorNameValue((UILabel(),
                                     labelFrame.offsetBy(dx: 0, dy: 50),
                                     director.name ?? "Unknown director name",
-                                    labelValueTextSize),
-            Label.tapToShowMore(TappableLabel(),
+                                    labelValueTextSize)),
+            Label.tapToShowMore((TappableLabel(),
                                 labelFrame.offsetBy(dx: 0, dy: 120),
                                 "Tap here to show more",
-                                labelTextSize),
-            Label.actorName(UILabel(),
+                                labelTextSize)),
+            Label.actorName((UILabel(),
                             labelFrame.offsetBy(dx: 0, dy: 120),
                             "Actor Name",
-                            labelTextSize),
-            Label.actorNameValue(UILabel(),
+                            labelTextSize)),
+            Label.actorNameValue((UILabel(),
                                  labelFrame.offsetBy(dx: 0, dy: 170),
                                  actor.name ?? "Unknown actor name",
-                                 labelValueTextSize),
-            Label.actorScreenName(UILabel(),
+                                 labelValueTextSize)),
+            Label.actorScreenName((UILabel(),
                                   labelFrame.offsetBy(dx: 0, dy: 220),
                                   "Actor Screen Name",
-                                  labelTextSize),
-            Label.actorScreenNameValue(UILabel(),
+                                  labelTextSize)),
+            Label.actorScreenNameValue((UILabel(),
                                        labelFrame.offsetBy(dx: 0, dy: 270),
                                        actor.screenName ?? "Unknown actor screen name",
-                                       labelValueTextSize)
+                                       labelValueTextSize))
             ]
             .map(makeALabel)
         
@@ -81,36 +83,40 @@ class DetailsInteractor: NSObject, DetailsInteractorInput, TappableLabelDelegate
     // MARK: Helpers
     
     private func makeALabel(_ label: Label) -> UILabel {
-        func makeLabel(_ label: UILabel, _ frame: CGRect, _ text: String, _ fontSize: CGFloat) -> UILabel {
-            let label = label
-            label.frame = frame
-            label.text = text
-            label.font = label.font.withSize(fontSize)
-            
-            return label
-        }
+        let labelParams: LabelParams
         
         switch label {
-        case let .directorName(label, frame, text, fontSize):
-            return makeLabel(label, frame, text, fontSize)
-        case let .directorNameValue(label, frame, text, fontSize):
-            return makeLabel(label, frame, text, fontSize)
-        case let .tapToShowMore(label, frame, text, fontSize):
-            label.delegate = self
-            return makeLabel(label, frame, text, fontSize)
-        case let .actorName(label, frame, text, fontSize):
-            label.isHidden = true
-            return makeLabel(label, frame, text, fontSize)
-        case let .actorNameValue(label, frame, text, fontSize):
-            label.isHidden = true
-            return makeLabel(label, frame, text, fontSize)
-        case let .actorScreenName(label, frame, text, fontSize):
-            label.isHidden = true
-            return makeLabel(label, frame, text, fontSize)
-        case let .actorScreenNameValue(label, frame, text, fontSize):
-            label.isHidden = true
-            return makeLabel(label, frame, text, fontSize)
+        case let .directorName(params):
+            labelParams = params
+        case let .directorNameValue(params):
+            labelParams = params
+        case let .tapToShowMore(params):
+            labelParams = params
+            (labelParams.0 as! TappableLabel).delegate = self
+        case let .actorName(params):
+            labelParams = params
+            labelParams.0.isHidden = true
+        case let .actorNameValue(params):
+            labelParams = params
+            labelParams.0.isHidden = true
+        case let .actorScreenName(params):
+            labelParams = params
+            labelParams.0.isHidden = true
+        case let .actorScreenNameValue(params):
+            labelParams = params
+            labelParams.0.isHidden = true
         }
+        
+        let label = labelParams.0
+        let frame = labelParams.1
+        let text = labelParams.2
+        let fontSize = labelParams.3
+        
+        label.frame = frame
+        label.text = text
+        label.font = label.font.withSize(fontSize)
+        
+        return label
     }
 
     func didReceiveTouch() {
